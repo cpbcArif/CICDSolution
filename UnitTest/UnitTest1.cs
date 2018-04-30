400193f697fddd1579a1ca278db367567b8d94f5
Arif Mosharraf <arif.mosharraf@bcpharmacists.org>
2018-04-30 14:25:17 -0700

    Sample Project to demonstrate CICD

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod()
         {
             Assert.IsTrue(true, "Success..");
         }
